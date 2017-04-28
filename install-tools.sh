eea496cb05743706d25c3d13b9ec825e2a1c0949
Juan Sepulveda <juan@sepulveda.do>
2017-04-27 22:24:56 -0400

    Update install tools script

--- install-tools.sh
+++ install-tools.sh
@@ -3,5 +3,8 @@ source ./console-log.sh
 bash install-nodejs.sh
 bash install-docker.sh
 bash install-spotify.sh
+bash install-R.sh
+bash install-rstudio.sh
+bash install-anaconda.sh
 
 consoleLog "All tools installed"
