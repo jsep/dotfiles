1f8a942460b8edc74bf1ce8702da35ef33fd1b1a
Juan Sepulveda <me@juan.do>
2021-11-29 11:08:57 -0400

    Install utils sh

---
+++ install-utils.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env zsh
+source ./env.sh
+
+# TODO install Rectangle
+  # https://rectangleapp.com
+  # Download app
+  # Install app
+  # Setup accessibility
+
+# TODO install vimium for safari
+
+# Install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
