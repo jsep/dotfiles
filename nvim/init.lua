12e0ab6c6e66f3792adf50a7c375cd99586a1a6c
Juan Sepulveda <me@juan.do>
2022-06-13 15:56:25 -0400

    Add nvim cmp

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,5 @@
 require "user.options"
+require "user.keymaps"
+require "user.plugins"
+require "user.colorscheme"
+require "user.cmp"
--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -14,6 +14,16 @@ function install_plugins(use)
   -- Colorschemes
   use "LunarVim/Colorschemes" 
 
+  -- cmp plugins
+  use "hrsh7th/nvim-cmp" -- The completion plugin
+  use "hrsh7th/cmp-buffer" -- buffer completions
+  use "hrsh7th/cmp-path" -- path completions
+  use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+
+  -- snippets
+  use "L3MON4D3/LuaSnip" --snippet engine
+  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 end
 
 
